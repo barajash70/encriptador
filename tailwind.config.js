533eaeb1a7c0682c9803870b53abdf5bdd9cdb23
Barajas <carmen-895@hotmail.com>
2022-10-03 18:05:23 -0500

    Estructura base

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        "light-grey": "#E5EEE5",
+        purple: "#7652C6",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
