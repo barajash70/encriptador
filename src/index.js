b9b6820560a31da98ecff3af612cda05f8df7b99
Barajas <carmen-895@hotmail.com>
2022-11-05 23:29:58 -0500

    Elaborando funciones

--- src/index.js
+++ src/index.js
@@ -1,64 +1,55 @@
 import "./css/style.css";
 import "./assets/logo-alura.svg";
 import "./assets/Vector.svg"
-// import "./assets/animation_1665196782581.json";
+import "./assets/animation_1665196782581.json";
 // import animation from "./assets/lottie.json";
 
-// const main = () => {
-//   console.log("Carmen");
-
-// const inputTexto = document.querySelector(".input-texto");
-// const mensaje = document.querySelector(".mensaje");
-// const animation = document.querySelector(".animation");
 
 
-// //   // Las "llaves" de encriptación que utilizaremos son las siguientes:
 
-// //   // `La letra "e" es convertida para "enter"`
-// //   //   `La letra "i" es convertida para "imes"`
-// //   //   `La letra "a" es convertida para "ai"`
-// //   //   `La letra "o" es convertida para "ober"`
-// //   //   `La letra "u" es convertida para "ufat"`
+const botonEncriptar = document.getElementById("buttonEncriptar");
+const botonDesencriptar = document.getElementById("buttonDesencriptar");
+const mensajeHTML = document.getElementById("mensaje");
+const textoHTML = document.getElementById("texto");
+const botonCopiar = document.getElementById("buttonCopy");
 
-// function btnEncriptar() {
-//   const textoEncriptado = buttonPlayElement(inputTexto.value)
-//   mensaje.value = textoEncriptado;
-//   mensaje.index.backgroundImage = "none"
-//   inputTexto.value = ""
-// }
+botonEncriptar.addEventListener("click", () => {
 
-// function encriptar(stringEncriptada) {
-//   let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-//   stringEncriptada = stringEncriptada.tolowerCase();
+    const inputTexto = document.querySelector(".input-texto");
 
-//   for (let i = 0; i < matrizCodigo.length; i++) {
-//     if (stringEncriptada.includes(matrizCodigo[i][0])) {
-//       stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-//     }
-//   }
-//   return stringEncriptada;
+    let textoArea = inputTexto.value;
 
-// }
+    let textoEncriptado = encriptar(textoArea);
 
-// let buttonPlayElement = document.getElementById("buttonPlay");
-// buttonPlayElement.addEventListener("click", () => {
+    mensajeHTML.textContent = textoEncriptado;
 
-//   let animationElement = document.getElementById("animation");
-//   let lottieAnimation = lottie.loadAnimation({
-//     container: animationElement, // the dom element that will contain the animation
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true,
-//     path: './assets/lottie.svg' // the path to the animation json
-//   });
+    textoHTML.classList.add("invisible");
 
 
-//   let buttonStopElement = document.getElementById("buttonStop");
-//   buttonStopElement.addEventListener("click", () => {
-//     console.dir(lottieAnimation);
 
-//     lottieAnimation.destroy();
-//   });
+
+    console.dir(botonCopiar);
+
+    // console.log(textoEncriptado);
+
+
+    // console.dir(inputTexto);
+
+});
+
+// botonDesencriptar.addEventListener("click", () => {
+
+//     const textoEncriptado = document.querySelector(".input-texto");
+
+//     let mensaje = textoEncriptado.value;
+
+//     let textoLeido = desencriptar(mensaje);
+
+
+
+
+
+//     // console.dir(textoLeido);
 // });
 
 
@@ -66,9 +57,47 @@ import "./assets/Vector.svg"
 
 
 
+const buttonCopy = document.querySelector(".buttonCopy");
+// const animation = document.querySelector(".animation");
 
 
-// main();
+// Las "llaves" de encriptación que utilizaremos son las siguientes:
+// `La letra "e" es convertida para "enter"`
+//   `La letra "i" es convertida para "imes"`
+//   `La letra "a" es convertida para "ai"`
+//   `La letra "o" es convertida para "ober"`
+//   `La letra "u" es convertida para "ufat"`
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+
+}
+
+
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+
+}
 
 
 
